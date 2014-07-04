ffcc98611bcaebf8fd177736f8593c21a1d97856
johnnyeven_work <johnnyeven@gmail.com>
2014-07-04 16:37:28 +0800

    1

--- application/controllers/order.php
+++ application/controllers/order.php
@@ -10,32 +10,42 @@ class Order extends CI_Controller
 		$raw_post_data = file_get_contents('php://input', 'r');
 		$inputParam = json_decode($raw_post_data);
 		
-		$receipt = $inputParam->receipt;
-		
-		if(!empty($receipt))
+		if(!empty($inputParam))
 		{
-			$parameter = array(
-					'receipt-data'	=>	$receipt
-			);
-			$parameter = json_encode($parameter);
+			$receipt = $inputParam->receipt;
 			
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $this->url);
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $parameter);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-					'Content-Type: application/json'
-			));
-			$result = curl_exec($ch);
-			
-			header('Content-type: application/json');
-			echo json_encode(array(
-				'command'	=>	'checkReceipt',
-				'code'		=>	1,
-				'receipt'	=>	$result
-			));
+			if(!empty($receipt))
+			{
+				$parameter = array(
+						'receipt-data'	=>	$receipt
+				);
+				$parameter = json_encode($parameter);
+				
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, $this->url);
+				curl_setopt($ch, CURLOPT_POST, 1);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $parameter);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+						'Content-Type: application/json'
+				));
+				$result = curl_exec($ch);
+				
+				header('Content-type: application/json');
+				echo json_encode(array(
+					'command'	=>	'checkReceipt',
+					'code'		=>	1,
+					'receipt'	=>	$result
+				));
+			}
+			else
+			{
+				echo json_encode(array(
+					'command'	=>	'checkReceipt',
+					'code'		=>	0
+				));
+			}
 		}
 		else
 		{
