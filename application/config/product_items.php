c781f126c5f600c2126dce20acd8ff17f518c35e
johnnyeven_work <johnnyeven@gmail.com>
2014-07-24 18:17:51 +0800

    1

--- application/config/product_items.php
+++ application/config/product_items.php
@@ -3,32 +3,44 @@
 $config['product_identifiers'] = array(
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack6",
-		'gold'					=>	2000,
+		'items'					=>	array(
+			'gold'					=>	2000
+		),
 		'sort'					=>	1
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack12",
-		'gold'					=>	5000,
+		'items'					=>	array(
+			'gold'					=>	5000
+		),
 		'sort'					=>	2
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack25",
-		'gold'					=>	12000,
+		'items'					=>	array(
+			'gold'					=>	12000
+		),
 		'sort'					=>	3
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack45",
-		'gold'					=>	25000,
+		'items'					=>	array(
+			'gold'					=>	25000
+		),
 		'sort'					=>	4
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack68",
-		'gold'					=>	40000,
+		'items'					=>	array(
+			'gold'					=>	40000
+		),
 		'sort'					=>	5
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack128",
-		'gold'					=>	100000,
+		'items'					=>	array(
+			'gold'					=>	100000
+		),
 		'sort'					=>	6
 	)
 );
