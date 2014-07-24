b9d4a1793fe602773cb3faf4e052f6582629bdb5
johnnyeven_work <johnnyeven@gmail.com>
2014-07-24 17:41:42 +0800

    1

--- application/config/product_items.php
+++ application/config/product_items.php
@@ -3,65 +3,41 @@
 $config['product_identifiers'] = array(
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack6",
+		'gold'					=>	2000,
 		'sort'					=>	1
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack12",
+		'gold'					=>	5000,
 		'sort'					=>	2
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack25",
+		'gold'					=>	12000,
 		'sort'					=>	3
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack45",
+		'gold'					=>	25000,
 		'sort'					=>	4
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack68",
+		'gold'					=>	40000,
 		'sort'					=>	5
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack128",
+		'gold'					=>	100000,
 		'sort'					=>	6
 	)
 );
 
 $config['product_actions'] = array(
-	'net.profzone.bacterial2.gold_pack6'	=>	array(
-		array(
-			'name'		=>	'gold',
-			'count'		=>	2000
-		)
-	),
-	'net.profzone.bacterial2.gold_pack12'	=>	array(
-		array(
-			'name'		=>	'gold',
-			'count'		=>	5000
-		)
-	),
-	'net.profzone.bacterial2.gold_pack25'	=>	array(
-		array(
-			'name'		=>	'gold',
-			'count'		=>	12000
-		)
-	),
-	'net.profzone.bacterial2.gold_pack45'	=>	array(
-		array(
-			'name'		=>	'gold',
-			'count'		=>	25000
-		)
-	),
-	'net.profzone.bacterial2.gold_pack68'	=>	array(
-		array(
-			'name'		=>	'gold',
-			'count'		=>	40000
-		)
-	),
-	'net.profzone.bacterial2.gold_pack128'	=>	array(
-		array(
-			'name'		=>	'gold',
-			'count'		=>	100000
-		)
-	)
+	'net.profzone.bacterial2.gold_pack6'	=>	2000,
+	'net.profzone.bacterial2.gold_pack12'	=>	5000,
+	'net.profzone.bacterial2.gold_pack25'	=>	12000,
+	'net.profzone.bacterial2.gold_pack45'	=>	25000,
+	'net.profzone.bacterial2.gold_pack68'	=>	40000,
+	'net.profzone.bacterial2.gold_pack128'	=>	100000
 );
