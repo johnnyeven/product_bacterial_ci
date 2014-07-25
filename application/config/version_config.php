8f2b95ea4e3b33e06be076a3593255848d8cc950
李翌文 <johnnyeven@gmail.com>
2014-07-25 23:08:11 +0800

    1

--- application/config/version_config.php
+++ application/config/version_config.php
@@ -4,26 +4,21 @@ $config['version_config'] = array(
 	'products'		=>	array(
 		'version'		=>	'20140710',
 		'command'		=>	'requestProductIds',
-		'url'			=>	'http://b.profzone.net/configuration/product_id'
+		'url'			=>	'http://b2.profzone.net/configuration/product_id'
 	),
 	'virtual_const'	=>	array(
 		'version'		=>	'20140708',
 		'command'		=>	'requestVirtualConst',
-		'url'			=>	'http://b.profzone.net/configuration/virtual'
-	),
-	'upgrade_const'	=>	array(
-		'version'		=>	'20140708',
-		'command'		=>	'requestUpgradeConst',
-		'url'			=>	'http://b.profzone.net/configuration/upgrade'
+		'url'			=>	'http://b2.profzone.net/configuration/virtual'
 	),
 	'score_board'	=>	array(
-		'version'		=>	'20140710',
+		'version'		=>	'20140725',
 		'command'		=>	'requestScoreBoard',
-		'url'			=>	'http://b.profzone.net/configuration/score_board'
+		'url'			=>	'http://b2.profzone.net/configuration/score_board'
 	),
 	'ad'	=>	array(
 		'version'		=>	'20140710',
 		'command'		=>	'requestAd',
-		'url'			=>	'http://b.profzone.net/configuration/ad'
+		'url'			=>	'http://b2.profzone.net/configuration/ad'
 	)
 );
