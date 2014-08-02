fc967bd1a0601a94442f1b35d2ed9b98421f940a
李翌文 <johnnyeven@gmail.com>
2014-08-02 17:03:49 +0800

    1

--- resources/script/activity.couponCode.js
+++ resources/script/activity.couponCode.js
@@ -21,5 +21,9 @@ $(function() {
 });
 
 function onGetCoupon(data) {
-	
+	if(data.code == 1001) {
+		$("#couponContent").html("<p>恭喜你获得下载码，请复制到安全的地方：" + data.download_code + "</p><p>注意：您无法再次获得该下载码，请妥善保管。</p>");
+	} else if(data.code == 1404) {
+		$("#couponContent").html("<p>暂时还没有发放下载码</p>");
+	}
 }
