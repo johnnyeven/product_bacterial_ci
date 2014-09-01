8c5ff530f6ffffe5dfe2bc0fdef5bfc5f31f6e4d
johnnyeven_work <johnnyeven@gmail.com>
2014-09-01 16:08:45 +0800

    1

--- application/config/product_items.php
+++ application/config/product_items.php
@@ -4,7 +4,10 @@ $config['product_identifiers'] = array(
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack6",
 		'items'					=>	array(
-			'gold'					=>	3000
+			'gold'					=>	3000,
+			'step'					=>	200,
+			'uper'					=>	5,
+			'killer'				=>	5
 		),
 		'sort'					=>	2
 	),
@@ -21,28 +24,40 @@ $config['product_identifiers'] = array(
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack25",
 		'items'					=>	array(
-			'gold'					=>	20000
+			'gold'					=>	20000,
+			'step'					=>	1000,
+			'uper'					=>	15,
+			'killer'				=>	15
 		),
 		'sort'					=>	3
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack45",
 		'items'					=>	array(
-			'gold'					=>	40000
+			'gold'					=>	40000,
+			'step'					=>	1500,
+			'uper'					=>	20,
+			'killer'				=>	20
 		),
 		'sort'					=>	4
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack68",
 		'items'					=>	array(
-			'gold'					=>	60000
+			'gold'					=>	60000,
+			'step'					=>	2000,
+			'uper'					=>	25,
+			'killer'				=>	25
 		),
 		'sort'					=>	5
 	),
 	array(
 		'productIdentifier'		=>	"net.profzone.bacterial2.gold_pack128",
 		'items'					=>	array(
-			'gold'					=>	150000
+			'gold'					=>	150000,
+			'step'					=>	3000,
+			'uper'					=>	50,
+			'killer'				=>	50
 		),
 		'sort'					=>	6
 	)
@@ -50,7 +65,10 @@ $config['product_identifiers'] = array(
 
 $config['product_actions'] = array(
 	'net.profzone.bacterial2.gold_pack6'	=>	array(
-		'gold'	=>	3000
+		'gold'		=>	3000,
+		'step'		=>	200,
+		'uper'		=>	5,
+		'killer'	=>	5
 	),
 	'net.profzone.bacterial2.gold_pack12'	=>	array(
 		'gold'		=>	8000,
@@ -59,15 +77,27 @@ $config['product_actions'] = array(
 		'killer'	=>	10
 	),
 	'net.profzone.bacterial2.gold_pack25'	=>	array(
-		'gold'	=>	20000
+		'gold'		=>	20000,
+		'step'		=>	1000,
+		'uper'		=>	15,
+		'killer'	=>	15
 	),
 	'net.profzone.bacterial2.gold_pack45'	=>	array(
-		'gold'	=>	35000
+		'gold'		=>	35000,
+		'step'		=>	1500,
+		'uper'		=>	20,
+		'killer'	=>	20
 	),
 	'net.profzone.bacterial2.gold_pack68'	=>	array(
-		'gold'	=>	60000
+		'gold'		=>	60000,
+		'step'		=>	2000,
+		'uper'		=>	25,
+		'killer'	=>	25
 	),
 	'net.profzone.bacterial2.gold_pack128'	=>	array(
-		'gold'	=>	150000
+		'gold'		=>	150000,
+		'step'		=>	3000,
+		'uper'		=>	40,
+		'killer'	=>	40
 	)
 );
--- application/config/version_config.php
+++ application/config/version_config.php
@@ -2,32 +2,32 @@
 
 $config['version_config'] = array(
 	'products'		=>	array(
-		'version'		=>	'20140901',
+		'version'		=>	'201409011',
 		'command'		=>	'requestProductIds',
 		'url'			=>	'http://b2.profzone.net/configuration/product_id'
 	),
 	'virtual_const'	=>	array(
-		'version'		=>	'20140901',
+		'version'		=>	'201409011',
 		'command'		=>	'requestVirtualConst',
 		'url'			=>	'http://b2.profzone.net/configuration/virtual'
 	),
 	'score_board'	=>	array(
-		'version'		=>	'20140901',
+		'version'		=>	'201409011',
 		'command'		=>	'requestScoreBoard',
 		'url'			=>	'http://b2.profzone.net/configuration/score_board'
 	),
 	'ad'			=>	array(
-		'version'		=>	'20140901',
+		'version'		=>	'201409011',
 		'command'		=>	'requestAd',
 		'url'			=>	'http://b2.profzone.net/configuration/ad'
 	),
 	'share_reward'	=>	array(
-		'version'		=>	'20140901',
+		'version'		=>	'201409011',
 		'command'		=>	'requestShareReward',
 		'url'			=>	'http://b2.profzone.net/configuration/share_reward'
 	),
 	'activity'		=>	array(
-		'version'		=>	'20140901',
+		'version'		=>	'201409011',
 		'command'		=>	'requestActivity',
 		'url'			=>	'http://b2.profzone.net/configuration/activity'
 	)
